d777f9bd97b2e9efba84de009b2e48e5242d315c
Sean Haughey <sdhaughey@gmail.com>
2016-07-06 17:57:37 -0500

    solution working for 2 hashes

--- pather.php
+++ pather.php
@@ -1,19 +1,20 @@
 <?php
-$handle = fopen("input.txt", "r");
-$string = file_get_contents('input.txt');
+$handle = fopen("input-3.txt", "r");
+$string = file_get_contents('input-3.txt');
 $hash_markers = [];
-//echo $string;
-if ($handle) {
-    while (($buffer = fgets($handle, 4096)) !== false) {
-        echo ($buffer) . '<br>';
-    }
-    if (!feof($handle)) {
-        echo "Error: unexpected fgets() fail\n";
-    }
-}
-echo '<br>';
+//echo input file
+// if ($handle) {
+//     while (($buffer = fgets($handle, 4096)) !== false) {
+//         echo ($buffer) . '<br>';
+//     }
+//     if (!feof($handle)) {
+//         echo "Error: unexpected fgets() fail\n";
+//     }
+// }
+// echo '<br>';
 $hash_loc = '0';
-$hash_count = -1;
+$hash_count = 0;
+// finds hash locations and saves to an array
 while($hash_loc !== false){
 	$hash_loc = strpos($string, '#', $hash_loc+1);
 	if($hash_loc){
@@ -21,10 +22,35 @@ while($hash_loc !== false){
 		$hash_count++;
 	}
 }
+echo "count: $hash_count<br>";
 foreach($hash_markers as $marker){
 	echo "marker: $marker<br>";
-	$string = substr_replace($string, '*', $marker+25, 1);
 }
-echo $string;
+
+$horizontal = ceil($hash_markers[1]/25) - ceil($hash_markers[0]/25);
+echo $horizontal . "<br>";
+for($i=0; $i<$horizontal; $i++){
+	$string = substr_replace($string, '*', $hash_markers[0]+25, 1);
+	$hash_markers[0] += 25;
+}
+echo "hash marker 0 = $hash_markers[0]<br>";
+
+$vertical = $hash_markers[1] - $hash_markers[0];
+echo "vertical = $vertical<br>";
+for($i=0; $i<$vertical-1; $i++){
+	$string = substr_replace($string, '*', $hash_markers[0]+1, 1);
+	$hash_markers[0] += 1;
+}
+
+// $string = strtr($string, ".", "\n");
+$arr = str_split($string);
+for($i=0; $i<count($arr); $i++){
+	if (ctype_space($arr[$i])){
+		$arr[$i] = "<br>";
+	}
+	echo $arr[$i];
+}
+//echo $string;
+file_put_contents("../solution.txt", $string);
 
 ?>
